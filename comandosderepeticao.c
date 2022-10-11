1159a2e9e98c35963b9fdc0d289997e83dcb12fd
acriles <115553419+acriles@users.noreply.github.com>
2022-10-11 10:24:08 -0300

    Update comandosderepeticao.c

--- comandosderepeticao.c
+++ comandosderepeticao.c
@@ -10,3 +10,22 @@ int x=0;
 return x;
 }
 
+//funcao primos 
+int primo(int n){
+int i;
+for(i=0; i<n; i++){
+if(n%i==0)break;
+} printf("o numero nao eh primo");
+}
+
+//funcao mdc
+int mdc(int a,int b){
+int m=0,i;
+do {
+ if(a%i==0 && b%i==0){
+m=i;
+ } 
+i++;
+} while (i<=a && i<=b);
+return m;
+}
